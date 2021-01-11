3bb5c0058a2e1661a9446d6d22d424cdbb3c5240
mafumafuultu <mafumafuultuForGit@gmail.com>
2021-01-11 17:19:49 +0900

    0.0.0

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # onloadjs
 onload promise
+
+
+```html
+<script type="module">
+import onload from './onload.js';
+
+onload().then( () => {
+	document.body;
+});
+</script>
+```
+
+## onload(document) => Promise
+
---
+++ test.js
@@ -0,0 +1,13 @@
+import test from 'ava';
+import jsdom from 'jsdom';
+import onload from "./onload.js";
+
+const {JSDOM} = jsdom;
+const dom = new JSDOM('');
+const {document} = dom.window;
+
+test('document.readyState is complete',async t => {
+	onload(document).then(() => {
+		t.is(document.readyState, 'complete');
+	})
+});
